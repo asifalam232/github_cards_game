f07cb7398858615df7ab5909a69bc1e8e59ee76b
Asif Alam <alamtheboss232@gmail.com>
2022-08-19 00:04:17 +0100

    Taking input from the user

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
@@ -16,7 +16,7 @@ const testData = [
 
 const CardList = (props) => (
     <div>
-        {testData.map(profile => <Card {...profile}/>)}
+        {props.profiles.map(profile => <Card {...profile}/>)}
     </div>
 );
 
@@ -35,12 +35,49 @@ class Card extends React.Component {
     }
 }
 
+class Form extends React.Component {
+    state = {userName: ''};
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(
+            this.state.userName
+        );
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    value={this.state.userName}
+                    onChange={event => this.setState({ userName: event.target.value })}
+                    placeholder={"Github username"}
+                    required
+                />
+                <button>Add Card</button>
+            </form>
+        );
+    }
+}
+
 class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         profiles: testData,
+    //     };
+    // }
+    state = {
+        profiles: testData
+    };
+
     render() {
         return (
             <>
                 <div className={"header"}>{this.props.title}</div>
-                <CardList />
+                <Form />
+                <CardList profiles={this.state.profiles}/>
             </>
         );
     }
