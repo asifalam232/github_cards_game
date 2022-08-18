e528f56e27bfe7a28d47458f0038e381ab14b2fd
Asif Alam <alamtheboss232@gmail.com>
2022-08-18 23:11:36 +0100

    Add github cards game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+const testData = [
+    {name: "Dan Abramov", avatar_url:
+            "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url:
+            "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url:
+            "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
+const CardList = (props) => (
+    <div>
+        {testData.map(profile => <Card {...profile}/>)}
+    </div>
+);
+
+class Card extends React.Component {
+    render() {
+        const profile = this.props;
+        return (
+            <div className="github-profile">
+                <img src={profile.avatar_url} />
+                <div className="info">
+                    <div className="name">{profile.name}</div>
+                    <div className="company">{profile.company}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <div className={"header"}>{this.props.title}</div>
+                <CardList />
+            </>
+        );
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <>
+            <App title="The Github Cards App"/>
+        </>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
